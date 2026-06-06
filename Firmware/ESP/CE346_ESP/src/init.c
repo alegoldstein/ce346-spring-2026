998f03d9acd0fea543e0a8ed48d38f1a3e19cae5
Alejandro Goldstein <alejandrogoldstein@dhcp-10-105-241-11.wireless.northwestern.private>
2026-06-05 22:45:00 -0500

    restore ESP CE346 code

---
+++ Firmware/ESP/CE346_ESP/src/init.c
@@ -0,0 +1,32 @@
+
+void init_uart(void){
+
+}
+
+void init_spi(void){
+
+}
+
+void init_scoreboard(){
+
+}
+
+//init game switch and kill switch buttons as interrupts
+void init_button_interrutps(){
+
+}
+
+//callback handler for kill switch button
+void kill_switch_handler(){
+
+}
+
+//handler for when game switch button is pressed
+void game_swtich_handler(){
+
+}
+
+//handler for when sound btn button is pressed
+void sound_btn_handler(){
+
+}
