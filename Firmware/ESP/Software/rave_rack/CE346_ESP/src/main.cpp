a34edcab86611d4df9689ca34002eedd3c8d83f0
Alejandro Goldstein <alejandrogoldstein@dhcp-10-105-241-11.wireless.northwestern.private>
2026-06-05 22:19:49 -0500

    more esp code

---
+++ Firmware/ESP/Software/rave_rack/CE346_ESP/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <FastLED.h>
+#include "init.h"
+#include "led.h"
+
+
+
+void setup() {
+
+
+  //init spi
+  
+
+
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
