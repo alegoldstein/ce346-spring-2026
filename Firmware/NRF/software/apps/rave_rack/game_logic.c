cdf85b56ebf00ec8217309d7322805ce2d8db9dd
Alejandro Goldstein <alejandrogoldstein@dhcp-10-105-241-11.wireless.northwestern.private>
2026-06-07 18:42:35 -0500

    adc interrupts

--- Firmware/NRF/software/apps/rave_rack/game_logic.c
+++ Firmware/NRF/software/apps/rave_rack/game_logic.c
@@ -7,50 +7,38 @@
 #include "nrfx_spim.h"
 #include "nrfx_saadc.h"
 
-// Button press callback function
-// You should not need to edit this, but you can if you want to
-static void gpio_handler(nrfx_gpiote_pin_t pin, nrf_gpiote_polarity_t _unused) {
-  // Overwrite the current button pressed as this one
-  // If multiple buttons have been pressed, only the most recent is saved
-  game_state.button_pressed = (uint8_t)pin;
-}
+// ADC channel configurations
+// These are ADC channel numbers that can be used in ADC calls
+#define PIEZO1_CH  6
+#define PIEZO2_CH 5
+#define PIEZO3_CH  4
+#define PIEZO4_CH 0
+#define SAADC_BASE_ADDR     0x40007000
+#define INTENSET_OFFSET     0x304
 
-static void adc_init(void) {
-  // Initialize the SAADC
-  nrfx_saadc_config_t saadc_config = {
-    .resolution = NRF_SAADC_RESOLUTION_12BIT,
-    .oversample = NRF_SAADC_OVERSAMPLE_DISABLED,
-    .interrupt_priority = 4,
-    .low_power_mode = false,
-  };
-  ret_code_t error_code = nrfx_saadc_init(&saadc_config, saadc_event_callback);
-  APP_ERROR_CHECK(error_code);
+//registers that show if the limit was passed
+#define CH0_LMTH_OFFSET     0x118
+#define CH4_LMTH_OFFSET     0x118 + (4 * 0x8)
+#define CH5_LMTH_OFFSET     0x118 + (5 * 0x8)
+#define CH6_LMTH_OFFSET     0x118 + (6 * 0x8)
+#define CH0_LMTH    (*(volatile uint32_t *)(SAADC_BASE_ADDR + CH0_LMTH_OFFSET))
+#define CH4_LMTH    (*(volatile uint32_t *)(SAADC_BASE_ADDR + CH4_LMTH_OFFSET))
+#define CH5_LMTH    (*(volatile uint32_t *)(SAADC_BASE_ADDR + CH5_LMTH_OFFSET))
+#define CH6_LMTH    (*(volatile uint32_t *)(SAADC_BASE_ADDR + CH6_LMTH_OFFSET))
 
-  // Initialize IR channel
-  nrf_saadc_channel_config_t temp_channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(ANALOG_TEMP_IN);
-  error_code = nrfx_saadc_channel_init(ADC_TEMP_CHANNEL, &temp_channel_config);
-  APP_ERROR_CHECK(error_code);
+//define the actual limits
+#define CH0_LIMIT_OFFSET    0x51C
+#define CH4_LIMIT_OFFSET    0x55C
+#define CH5_LIMIT_OFFSET    0x56C
+#define CH6_LIMIT_OFFSET    0x57C
+#define CH0_LIMIT    (*(volatile uint32_t *)(SAADC_BASE_ADDR + CH0_LIMIT_OFFSET))
+#define CH4_LIMIT    (*(volatile uint32_t *)(SAADC_BASE_ADDR + CH4_LIMIT_OFFSET))
+#define CH5_LIMIT    (*(volatile uint32_t *)(SAADC_BASE_ADDR + CH5_LIMIT_OFFSET))
+#define CH6_LIMIT    (*(volatile uint32_t *)(SAADC_BASE_ADDR + CH6_LIMIT_OFFSET))
 
-  // Initialize PIEZO_1 channel
-  nrf_saadc_channel_config_t temp_channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(ANALOG_TEMP_IN);
-  error_code = nrfx_saadc_channel_init(ADC_TEMP_CHANNEL, &temp_channel_config);
-  APP_ERROR_CHECK(error_code);
+#define SAADC_INTENSET    (*(volatile uint32_t *)(SAADC_BASE_ADDR + INTENSET_OFFSET))
 
-  // Initialize PIEZO_2 channel
-  nrf_saadc_channel_config_t light_channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(ANALOG_LIGHT_IN);
-  error_code = nrfx_saadc_channel_init(ADC_LIGHT_CHANNEL, &light_channel_config);
-  APP_ERROR_CHECK(error_code);
-
-   // Initialize PIEZO_3 channel
-  nrf_saadc_channel_config_t light_channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(ANALOG_LIGHT_IN);
-  error_code = nrfx_saadc_channel_init(ADC_LIGHT_CHANNEL, &light_channel_config);
-  APP_ERROR_CHECK(error_code);
-
-   // Initialize PIEZO_4 channel
-  nrf_saadc_channel_config_t light_channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(ANALOG_LIGHT_IN);
-  error_code = nrfx_saadc_channel_init(ADC_LIGHT_CHANNEL, &light_channel_config);
-  APP_ERROR_CHECK(error_code);
-}
+acd_init();
 
 void init_game(game_state_t *game, nrfx_spim_t *spim_inst){
   // Set pin directions
@@ -86,4 +74,77 @@ void init_game(game_state_t *game, nrfx_spim_t *spim_inst){
   nrfx_spim_init(spim_inst, &spim_config, NULL, NULL);
 
   // Initialize SAADC
+  adc_init();
+}
+
+
+static void adc_init(void) {
+  // Initialize the SAADC
+  nrfx_saadc_config_t saadc_config = {
+    .resolution = NRF_SAADC_RESOLUTION_12BIT,
+    .oversample = NRF_SAADC_OVERSAMPLE_DISABLED,
+    .interrupt_priority = 4,
+    .low_power_mode = false,
+  };
+  ret_code_t error_code = nrfx_saadc_init(&saadc_config, saadc_event_callback);
+  APP_ERROR_CHECK(error_code);
+
+  // Initialize piezo1 channel
+  nrf_saadc_channel_config_t piezo1_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(PIEZO1);
+  error_code = nrfx_saadc_channel_init(PIEZO1_CH, &piezo1_config);
+  APP_ERROR_CHECK(error_code);
+
+  // Initialize piezo2 channel
+  nrf_saadc_channel_config_t piezo2_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(PIEZO2);
+  error_code = nrfx_saadc_channel_init(PIEZO2_CH, &piezo2_config);
+  APP_ERROR_CHECK(error_code);
+
+   // Initialize piezo3 channel
+  nrf_saadc_channel_config_t piezo3_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(PIEZO3);
+  error_code = nrfx_saadc_channel_init(PIEZO3_CH, &piezo3_config);
+  APP_ERROR_CHECK(error_code);
+
+   // Initialize piezo4 channel
+  nrf_saadc_channel_config_t piezo4_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(PIEZO4);
+  error_code = nrfx_saadc_channel_init(PIEZO4_CH, &piezo4_config);
+  APP_ERROR_CHECK(error_code);
+
+  //enable saadc limit high interrupts for ch 6,5,4,0
+  SAADC_INTENSET = (1 << 19) | (1 << 17) | (1<<15) | (1 << 7);
+
+  CH0_LIMIT = adc_limit << 8;
+  CH4_LIMIT = adc_limit << 8;
+  CH5_LIMIT = adc_limit << 8;
+  CH6_LIMIT = adc_limit << 8;
+
+  NVIC_EnableIRQ(SAADC_IRQn);
+  NVIC_SetPriority(SAADC_IRQn, 7);
+}
+
+void saadc_event_callback(nrfx_saadc_evt_t const * p_event)
+{
+    corners = 0;
+    //ch0 piezo correlates to leftmost bits (bottom left rn)
+    if (CH0_LMTH == 1){
+    CH0_LMTH = 0;
+    corners |= 3 << 6;
+    }
+
+    //ch4 piezo correlates to middle left bits (bottom right rn)
+    if (CH4_LMTH == 1){
+    CH4_LMTH = 0;
+    corners |= 3 << 4;
+    }
+
+    //ch5 piezo correlates to middle right bits (top left)
+    if (CH5_LMTH == 1){
+    CH5_LMTH = 0;
+    corners |= 3 << 2;
+    }
+
+    //ch6 piezo correlates to right bits (top right)
+    if (CH6_LMTH == 1){
+    CH6_LMTH = 0;
+    corners |= 3;
+    }
 }
--- Firmware/NRF/software/apps/rave_rack/main.c
+++ Firmware/NRF/software/apps/rave_rack/main.c
@@ -6,24 +6,18 @@
 #include "nrf_delay.h"
 #include "microbit_v2.h"
 
-// These are GPIO pin numbers that can be used in ADC configurations
-// AIN1 is breakout pin 1. AIN2 is breakout pin 2.
-#define ANALOG_TEMP_IN  NRF_SAADC_INPUT_AIN1
-#define ANALOG_LIGHT_IN NRF_SAADC_INPUT_AIN2
-
-// ADC channel configurations
-// These are ADC channel numbers that can be used in ADC calls
-#define ADC_TEMP_CHANNEL  0
-#define ADC_LIGHT_CHANNEL 1
 
 game_state_t game;
 nrfx_spim_t spim_inst;
+uint8_t adc_limit = 0x3F;
 uint8_t esp_data_buf = {0};
+uint8_t corners = 0; //2 left bits = bottom left, 2 left middle bits = bottom right, 2 right middle bits = top left, 2 right bits = top right
 bool send_audio = false;
 
 int main(void) {
     //set game defaults
     init_game(&game, &spim_inst);
+    adc_init();
 
     //main loop
     while(1) {
@@ -48,13 +42,13 @@ int main(void) {
             game.TEAM_1_SCORE = count;
 
             //communicate to ESP
-            //example spi transfer
                 // Perform a SPI transmission (COPI)
                 uint8_t data[4] = {0x00, 0x0F, 0xF0, 0xFF};
                 nrfx_spim_xfer_desc_t data_xfer = NRFX_SPIM_XFER_TX(&data, 4);
                 nrfx_spim_xfer(&SPIM_INST, &data_xfer, 0);
         }
         if (send_audio){
+            //prepare data buffer with audio
 
         }
 
@@ -63,35 +57,3 @@ int main(void) {
 }
 
 
-
-static void adc_init(void) {
-  // Initialize the SAADC
-  nrfx_saadc_config_t saadc_config = {
-    .resolution = NRF_SAADC_RESOLUTION_12BIT,
-    .oversample = NRF_SAADC_OVERSAMPLE_DISABLED,
-    .interrupt_priority = 4,
-    .low_power_mode = false,
-  };
-  ret_code_t error_code = nrfx_saadc_init(&saadc_config, saadc_event_callback);
-  APP_ERROR_CHECK(error_code);
-
-  // Initialize piezo1 channel
-  nrf_saadc_channel_config_t temp_channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(ANALOG_TEMP_IN);
-  error_code = nrfx_saadc_channel_init(ADC_TEMP_CHANNEL, &temp_channel_config);
-  APP_ERROR_CHECK(error_code);
-
-  // Initialize piezo2 channel
-  nrf_saadc_channel_config_t light_channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(ANALOG_LIGHT_IN);
-  error_code = nrfx_saadc_channel_init(ADC_LIGHT_CHANNEL, &light_channel_config);
-  APP_ERROR_CHECK(error_code);
-
-   // Initialize piezo3 channel
-  nrf_saadc_channel_config_t light_channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(ANALOG_LIGHT_IN);
-  error_code = nrfx_saadc_channel_init(ADC_LIGHT_CHANNEL, &light_channel_config);
-  APP_ERROR_CHECK(error_code);
-
-   // Initialize piezo4 channel
-  nrf_saadc_channel_config_t light_channel_config = NRFX_SAADC_DEFAULT_CHANNEL_CONFIG_SE(ANALOG_LIGHT_IN);
-  error_code = nrfx_saadc_channel_init(ADC_LIGHT_CHANNEL, &light_channel_config);
-  APP_ERROR_CHECK(error_code);
-}
